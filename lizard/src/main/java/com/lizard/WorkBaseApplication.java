753d2505031f609550670fa5ce30202b1c70959b
jm.hong <jm.hong@kakaocorp.comm>
2023-09-29 19:06:26 +0900

    refactor
    이름변경.

--- lizard/src/main/java/com/lizard/WorkBaseApplication.java
+++ lizard/src/main/java/com/lizard/WorkBaseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PloggingApplication {
+public class WorkBaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PloggingApplication.class, args);
+		SpringApplication.run(WorkBaseApplication.class, args);
 	}
 
 }
