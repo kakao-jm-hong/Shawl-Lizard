457c6fb76da72c432aef1762e4aff2dd7639dd88
jm.hong <jm.hong@kakaocorp.comm>
2023-09-30 16:43:27 +0900

    feat bash 수정

--- lizard/run_vue.sh
+++ lizard/run_vue.sh
@@ -3,6 +3,8 @@ if [ "$1" == "serve" ]; then
   cd ./src/front && npm run serve
 elif [ "$1" == "build" ]; then
   cd ./src/front && npm run build
+elif [ "$1" == "install" ]; then
+  cd ./src/front && npm run install
 else
   echo "Invalid option: $1"
   exit 1
